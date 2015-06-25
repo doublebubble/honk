d456f2eba32acba962155e9ae6ed5d285f491837
harpreet4u <happysinghsingh7@gmail.com>
2015-06-25 21:03:51 +0530

    initial audio recorder cut

--- app/src/main/java/com/gethonk/honk/MainActivity.java
+++ app/src/main/java/com/gethonk/honk/MainActivity.java
@@ -1,39 +1,126 @@
 package com.gethonk.honk;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    private Button mButton;
+    private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
+    private static final String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp4";
+    private static final String AUDIO_RECORDER_FOLDER = "Honk";
+    private MediaRecorder recorder = null;
+    private int currentFormat = 0;
+    private int outputFormats[] = {MediaRecorder.OutputFormat.MPEG_4, MediaRecorder.OutputFormat.THREE_GPP};
+    private String[] fileExtensions = {AUDIO_RECORDER_FILE_EXT_MP4, AUDIO_RECORDER_FILE_EXT_3GP};
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        mButton = (Button) findViewById(R.id.button1);
+        if (!hasMicrophone()) {
+            mButton.setEnabled(false);
+            Toast.makeText(this, "Microphone not available on device", Toast.LENGTH_SHORT).show();
+        }else {
+            mButton.setEnabled(true);
         }
+        mButton.setOnTouchListener(new View.OnTouchListener() {
 
-        return super.onOptionsItemSelected(item);
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Log.e(LOG_TAG, "Start Recording");
+                        startRecording();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.e(LOG_TAG, "Stop Recording");
+                        stopRecording();
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private String getFileName() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath, AUDIO_RECORDER_FOLDER);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        return (file.getAbsolutePath() + "/audio_" + System.currentTimeMillis() + fileExtensions[currentFormat]);
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(outputFormats[currentFormat]);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(getFileName());
+        recorder.setOnErrorListener(errorListener);
+        recorder.setOnInfoListener(infoListener);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IllegalStateException e) {
+            Log.e(LOG_TAG, e.toString());
+        } catch (IOException e) {
+            Log.e(LOG_TAG, e.toString());
+        }
+    }
+
+    private MediaRecorder.OnErrorListener errorListener = new MediaRecorder.OnErrorListener() {
+        @Override
+        public void onError(MediaRecorder mr, int what, int extra) {
+            Log.e(LOG_TAG, "Error: " + what + ", " + extra);
+        }
+    };
+
+    private MediaRecorder.OnInfoListener infoListener = new MediaRecorder.OnInfoListener() {
+        @Override
+        public void onInfo(MediaRecorder mr, int what, int extra) {
+            Log.e(LOG_TAG, "Warning: " + what + ", " + extra);
+        }
+    };
+
+    private void stopRecording() {
+        if (null != recorder) {
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+            recorder = null;
+        }
+    }
+
+    protected boolean hasMicrophone() {
+        PackageManager mPackageManager = this.getPackageManager();
+        return mPackageManager.hasSystemFeature(
+                PackageManager.FEATURE_MICROPHONE);
     }
 }
+
+
