c88265d8442d8eb5be4af78344fd0804211b04c2
lafolle <fghjklp@gmail.com>
2015-07-03 09:51:46 +0530

    setup database

--- app/src/main/java/com/gethonk/honk/AlarmContract.java
+++ app/src/main/java/com/gethonk/honk/AlarmContract.java
@@ -7,9 +7,43 @@ import android.provider.BaseColumns;
  */
 public class AlarmContract {
 
+    // To prevent someone from accedentally instantiating it, give it
+    // an empty contructor
+    public AlarmContract(){}
+
     public static final class AlarmEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "alarm";
+
+        // time at which alarm was set
+        public static final String COLUMN_NAME_SET_TIME = "st";
+
+        // hour at which alarm will go off
+        public static final String COLUMN_NAME_ALARM_HOUR = "h";
+
+        // minute at which alarm will go off
+        public static final String COLUMN_NAME_ALARM_MIN = "m";
+
+        // for a given instance of alarm being set off, after how much
+        // time should it repeat itself. (minutes)
+        public static final String COLUMN_NAME_REPEAT_AFTER = "ra";
+
+        // for a given instance of alarm being set off, how many times should
+        // it repeat itself.
+        public static final String COLUMN_NAME_REPEAT_COUNT = "rc";
+
+        // should the alarm explode everyday, weekday, weekend or selected
+        // days (comma seperated strings)
+        public static final String COLUMN_NAME_ACTIVE_RANGE = "ar";
+
+        // what sound needs to be played when alarm is set off
+        public static final String COLUMN_NAME_SOUND = "s";
+
+        // name of alarm
+        public static final String COLUMN_NAME_NAME = "n";
+
+        // whether alarm is ready to go off at next activerange or not
+        public static final String COLUMN_NAME_IS_ACTIVE = "ia";
     }
 
 }
--- app/src/main/java/com/gethonk/honk/HonkDbHelper.java
+++ app/src/main/java/com/gethonk/honk/HonkDbHelper.java
@@ -3,16 +3,36 @@ package com.gethonk.honk;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.gethonk.honk.AlarmContract.AlarmEntry;
 
 /**
  * Created by karan on 01/07/15.
  */
 public class HonkDbHelper extends SQLiteOpenHelper {
 
-    // if database schema is changed, increment
+    // If database schema is changed, increment this.
     private static final int DATABASE_VERSION = 0;
 
-    static final String DATABASE_NAME = "honk.db";
+    private static final String DATABASE_NAME = "honk.db";
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String DATETIME_TYPE = " DATETIME";
+    private static final String INTEGER_TYPE = " INTEGER";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_ENTRIES =
+                "CREATE TABLE " + AlarmEntry.TABLE_NAME + " (" +
+                        AlarmEntry._ID + " INTEGER PRIMARY KEY " + COMMA_SEP +
+                        AlarmEntry.COLUMN_NAME_NAME + TEXT_TYPE + COMMA_SEP +
+                        AlarmEntry.COLUMN_NAME_SET_TIME + DATETIME_TYPE + COMMA_SEP +
+                        AlarmEntry.COLUMN_NAME_ALARM_HOUR + INTEGER_TYPE + COMMA_SEP +
+                        AlarmEntry.COLUMN_NAME_ALARM_MIN + INTEGER_TYPE + COMMA_SEP +
+                        AlarmEntry.COLUMN_NAME_IS_ACTIVE + INTEGER_TYPE + COMMA_SEP +
+                        AlarmEntry.COLUMN_NAME_REPEAT_AFTER + INTEGER_TYPE + COMMA_SEP +
+                        AlarmEntry.COLUMN_NAME_REPEAT_COUNT + INTEGER_TYPE + COMMA_SEP +
+                        AlarmEntry.COLUMN_NAME_SOUND + TEXT_TYPE + COMMA_SEP +
+                        AlarmEntry.COLUMN_NAME_ACTIVE_RANGE + TEXT_TYPE + " )";
+    private static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + AlarmEntry.TABLE_NAME;
 
     public HonkDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,14 +40,13 @@ public class HonkDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-        final String SQL_CREATE_ALARM_TABLE = "CREATE TABLE ";
-
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(sqLiteDatabase);
     }
 }
 
